580cdc5e06779176ede9e6978c0c150c9b56cb3f
fmtorino <66545772+fmtorino@users.noreply.github.com>
2023-06-28 13:37:00 +0800

    Added Allowed Host

--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '172.16.4.22'
+]
 
 
 # Application definition
